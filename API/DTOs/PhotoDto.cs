e133545faa25f313cc7901dd270e72f4e4c96947
szabii70 <szabii70@stud.uni-obuda.hu>
2023-12-22 11:45:03 +0100

    End of section 8

---
+++ API/DTOs/PhotoDto.cs
@@ -0,0 +1,10 @@
+﻿namespace API;
+
+public class PhotoDto
+{
+    public int Id { get; set; }
+    public string Url { get; set; }
+    public bool IsMain{ get; set; }
+    public string PublicId { get; set; }
+    public int AppUserId { get; set; }
+}
